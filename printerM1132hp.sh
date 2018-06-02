7eaf1b7ddc05ab1f9328804e00d695749a95ea55
YUri <yuri@dcc.ufrj.br>
2018-06-01 23:56:48 -0300

    color change

--- printerM1132hp.sh
+++ printerM1132hp.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
+export red='\033[1;31m'
+export NC='\033[0m'
+#nocolor nc
+export green='\033[0;32m' 
 
 #update system
-echo "Updating system"
-sudo pacman -Syu
+echo -e  "${green}Updating system${NC}"
+#sudo pacman -Syu
 
 #checking if pacaur exists
+echo -e "${green}Using pacaur or yaourt${NC}"
 pacman -Qqe | grep 'pacaur' &> /dev/null
 if [ $? == 0 ];then
-	echo "Installing cups through pacaur"
-	pacaur -S cups cups-pdf cups-filters 
+	echo -e "--Installing cups through pacaur"
+#	pacaur -S cups cups-pdf cups-filters 
 else
-	echo "Installing cups through yaourt"
-	yaourt -S cups cups-pdf cups-filters 
+	echo -e "--Installing cups through yaourt"
+#	yaourt -S cups cups-pdf cups-filters 
 fi
 
 #enabling cups and adding user to group
-sudo systemctl enable org.cups.cupsd
-sudo systemctl status org.cups.cupds
+echo -e "${green}Enabling cups${NC}"
+#sudo systemctl enable org.cups.cupsd
+#sudo systemctl status org.cups.cupds
 
-sudo gpasswd -a $(whoami) lp
-sudo pacman -S foomatic-db foomatic-db-engine wget python-gobject 
+#sudo gpasswd -a $(whoami) lp
+#sudo pacman -S foomatic-db foomatic-db-engine wget python-gobject 
 
-pacaur -S hplip
-printf '0\nd\ny\nm\ny\n\ny\n' | sudo  hp-setup -i 
-echo "PRINTER INSTALLED"
+#pacaur -S hplip
+
+#printf '0\nd\ny\nm\ny\n\ny\n' | sudo  hp-setup -i 
+echo -e  "${green}PRINTER INSTALLED${NC}"
 echo "Test page will be printed"
+echo "To remove a printer just run : sudo lpadmin -x PrinterName"
+echo "To manage printer and spooler  just access cups on: http://localhost:631/"
+
